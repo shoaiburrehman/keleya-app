39864cf99355d326f2dd37d947d7e1791dbe3c62
Shoaib Ur Rehman Khan <shoaiburrehman249@gmail.com>
2023-09-02 12:34:07 +0500

    images fix

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+export const icons = {
+  authBackgroundImg: require('./images/authentication-background-image.jpg'),
+  couchSmile: require('./images/couch_smile.jpg'),
+  dueDateBackgroundImg: require('./images/due-date-background-image.jpg'),
+  firstIntroImg: require('./images/first-intro-image.jpg'),
+  logo: require('./images/keleya-logo.png'),
+  notifBgImg: require('./images/notifications-background-image.jpg'),
+  workoutBgImg: require('./images/workout-goal-background-image.jpg'),
+};
